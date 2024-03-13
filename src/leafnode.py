91a636812d88491359d631e2e27d6cd10a644118
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-13 18:09:29 -0300

    refactor: clean code

--- src/leafnode.py
+++ src/leafnode.py
@@ -3,7 +3,7 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
     def __init__(self, tag=None, value=None, props=None):
-        super().__init__(tag=tag, value=value, children=None, props=props)
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
 
