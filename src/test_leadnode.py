a803a9d4d0fde5dd24bccb07cf463ff297ee61d1
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-13 17:18:09 -0300

    add leafnode test cases

---
+++ src/test_leadnode.py
@@ -0,0 +1,29 @@
+
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestTextNode(unittest.TestCase):
+
+    def test_tag(self):
+        n = LeafNode(tag="p", value="Text in paragraph", props={"id": "main"})
+        self.assertEqual(n.tag, 'p')
+
+    def test_value(self):
+        n = LeafNode(tag="p", value="Text in paragraph", props={"id": "main"})
+        self.assertEqual(n.value, 'Text in paragraph')
+
+    def test_props(self):
+        n = LeafNode(tag="p", value="Text in paragraph", props={"id": "main"})
+        self.assertEqual(n.props, {"id": "main"})
+
+    def test_props_to_html(self):
+        n = LeafNode(tag="p", value="Text in paragraph", props={"id": "main"})
+        n.props["href"] = "app.com"
+        self.assertEqual(
+            n.to_html(), '<p id="main" href="app.com">Text in paragraph</p>')
+
+
+if __name__ == "__main__":
+    unittest.main()
