212623c8d8293c22594b9681b7f20fc279f5b20a
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-12 21:14:56 -0300

    add htmlnode class

---
+++ src/htmlnode.py
@@ -0,0 +1,33 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        res = ''
+        if self.props is None:
+            print("HTMLNode's props is None")
+            return
+        for key in self.props:
+            res += key + '="' + self.props[key] + '" '
+        return res[:-1]
+
+    def __repr__(self):
+        res = ''
+        if self.tag:
+            res += 'tag: ' + self.tag + '\n'
+        if self.value:
+            res += 'value: ' + self.value + '\n'
+        if self.children:
+            res += 'children:\n{\n'
+            for child in self.children:
+                res += repr(child)
+            res += '}\n'
+        if self.props:
+            res += 'props: ' + self.props_to_html() + '\n'
+        return res
