8336d0582aee92b68a50ca01e8eaa88d4a9389ff
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-14 10:40:52 -0300

    add parentnode class

---
+++ src/parentnode.py
@@ -0,0 +1,18 @@
+from htmlnode import HTMLNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode's tag is None")
+
+        res = '<' + self.tag + '>'
+
+        for child in self.children:
+            res += child.to_html()
+
+        res += '</' + self.tag + '>'
+        return res
