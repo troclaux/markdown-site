bfa550336a70a728bc664413ef1fcb9dd4f3430c
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-14 14:11:59 -0300

    feat: add function that converts TextNode to LeafNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,6 @@
+from leafnode import LeafNode
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -16,3 +19,21 @@ class TextNode:
 
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
+
+
+def text_node_to_html_node(text_node):
+    # text_type = ["text", "bold", "italic", "code", "link", "image"]
+    if text_node.text_type == "text":
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == "bold":
+        return LeafNode('b', text_node.text)
+    if text_node.text_type == "italic":
+        return LeafNode('i', text_node.text)
+    if text_node.text_type == "code":
+        return LeafNode('code', text_node.text)
+    if text_node.text_type == "link":
+        return LeafNode('a', text_node.text, {"href": text_node.url})
+    if text_node.text_type == "image":
+        prop_dict = {"src": text_node.url, "alt": text_node.text}
+        return LeafNode('img', '', prop_dict)
+    raise Exception(f"Invalid text type: {text_node.text_type}")
