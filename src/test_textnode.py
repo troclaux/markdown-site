a500cc30e46a878180af2e5dba8893c636b24a1e
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-04-04 12:06:10 -0300

    test: add test cases for function text_to_textnodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -153,5 +153,26 @@ class TestTextNode(unittest.TestCase):
             new_nodes,
         )
 
+    def test_text_to_textnodes(self):
+        input = 'This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)'
+        expected = [
+            TextNode("This is ", text_type_text),
+            TextNode("text", text_type_bold),
+            TextNode(" with an ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" word and a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" and an ", text_type_text),
+            TextNode(
+                "image",
+                text_type_image,
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"
+            ),
+            TextNode(" and a ", text_type_text),
+            TextNode("link", text_type_link, "https://boot.dev"),
+            ]
+        res = text_to_textnodes(input)
+        self.assertListEqual(expected, res)
+
 if __name__ == "__main__":
     unittest.main()
