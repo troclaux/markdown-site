58f7c2c2672a6eaf9fb86f280c35e046f83bcfe4
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-04-05 09:00:40 -0300

    feat: add function that split strings into blocks

--- src/extract_markdown.py
+++ src/extract_markdown.py
@@ -7,3 +7,13 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+
+def markdown_to_blocks(markdown):
+    res = []
+    blocks = markdown.split("\n\n")
+    for block in blocks:
+        if block == "":
+            continue
+        res.append(block.strip())
+    return res
