15866d95e9e6ec754846a799e73d3524bfcee4a9
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-04-06 21:33:02 -0300

    remove markdown_to_blocks function

--- src/extract_markdown.py
+++ src/extract_markdown.py
@@ -7,13 +7,3 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"\[(.*?)\]\((.*?)\)", text)
-
-
-def markdown_to_blocks(markdown):
-    res = []
-    blocks = markdown.split("\n\n")
-    for block in blocks:
-        if block == "":
-            continue
-        res.append(block.strip())
-    return res
