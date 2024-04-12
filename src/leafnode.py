e052e5b2cf5acd598273c74a4328485493dbdbd5
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-04-12 10:52:53 -0300

    chore: standardize string quotes and improve handling of None in HTMLNode methods

--- src/leafnode.py
+++ src/leafnode.py
@@ -6,18 +6,17 @@ class LeafNode(HTMLNode):
         super().__init__(tag=tag, value=value, children=None, props=props)
 
     def to_html(self):
-
         if self.value is None:
             raise ValueError("leaf node's value is None")
 
         if self.tag is None:
             return self.value
 
-        html_str = f'<{self.tag}'
+        html_str = f"<{self.tag}"
 
         if self.props is not None:
             for prop_key in self.props:
                 html_str += f' {prop_key}="{self.props[prop_key]}"'
 
-        html_str += f'>{self.value}</{self.tag}>'
+        html_str += f">{self.value}</{self.tag}>"
         return html_str
