ffea6376f56b5ee42150983d24d04d1d822551b8
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-04-13 15:26:46 -0300

    feat: add function to recursively copy content from one directory to another

--- src/main.py
+++ src/main.py
@@ -1,7 +1,35 @@
-from textnode import TextNode
+import os
+import shutil
 
-print('hello world')
+from textnode import *
+
+# os.path.exists
+# os.listdir
+# os.path.join
+# os.path.isfile
+# os.mkdir
+# shutil.copy
+# shutil.rmtree
+
+
+def copy_dir_content(src, dest):
+    if os.path.exists(dest):
+        print(f"removing {dest} folder")
+        shutil.rmtree(dest)
+    print(f"creating {dest} folder")
+    os.mkdir(dest)
+    for item in os.listdir(src):
+        source_item_path = f"{src}/{item}"
+        if os.path.isfile(source_item_path):
+            print(f"copying {source_item_path} to {dest}")
+            shutil.copy(source_item_path, dest)
+        else:
+            print(f"copying {source_item_path} directory to {dest}")
+            copy_dir_content(source_item_path, f"{dest}/{item}")
 
 
 if __name__ == "__main__":
-    node = TextNode('This is a text node, bold, https://www.boot.dev')
+    source = "./public"
+    destination = "./test"
+
+    copy_dir_content(source, destination)
