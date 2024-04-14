9885caea715429fbcea2da8f509389501667373a
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-04-14 14:43:30 -0300

    fix: parentnode's to_html() error

--- src/parentnode.py
+++ src/parentnode.py
@@ -5,6 +5,10 @@ class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag=tag, value=None, children=children, props=props)
 
+    def __iter__(self):
+        for child in self.children:
+            yield child
+
     def to_html(self):
         if self.tag is None:
             raise ValueError("ParentNode's tag is None")
