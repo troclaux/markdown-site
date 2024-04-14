16cc888625712af20ebb6833dfbd8605a8bf6902
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-04-14 17:32:56 -0300

    feat: move copy_dir_content() to generate_html_page

--- src/generate_html_page.py
+++ src/generate_html_page.py
@@ -1,4 +1,37 @@
+import os
+import shutil
+
 from block_functions import *
+
+
+# os.path.exists
+# os.listdir
+# os.path.join
+# os.path.isfile
+# os.mkdir
+# shutil.copy
+# shutil.rmtree
+
+# os.listdir
+# os.path.join
+# os.path.isfile
+# pathlib.Path
+
+
+def copy_dir_content(src, dest):
+    if os.path.exists(dest):
+        print(f"removing {dest} folder")
+        shutil.rmtree(dest)
+    print(f"creating {dest} folder")
+    os.mkdir(dest)
+    for item in os.listdir(src):
+        source_item_path = f"{src}/{item}"
+        if os.path.isfile(source_item_path):
+            print(f"copying {source_item_path} to {dest}")
+            shutil.copy(source_item_path, dest)
+        else:
+            print(f"copying {source_item_path} directory to {dest}")
+            copy_dir_content(source_item_path, f"{dest}/{item}")
 
 
 def extract_title(markdown):
--- src/main.py
+++ src/main.py
@@ -1,32 +1,5 @@
-import os
-import shutil
-
+from generate_html_page import *
 from textnode import *
-
-# os.path.exists
-# os.listdir
-# os.path.join
-# os.path.isfile
-# os.mkdir
-# shutil.copy
-# shutil.rmtree
-
-
-def copy_dir_content(src, dest):
-    if os.path.exists(dest):
-        print(f"removing {dest} folder")
-        shutil.rmtree(dest)
-    print(f"creating {dest} folder")
-    os.mkdir(dest)
-    for item in os.listdir(src):
-        source_item_path = f"{src}/{item}"
-        if os.path.isfile(source_item_path):
-            print(f"copying {source_item_path} to {dest}")
-            shutil.copy(source_item_path, dest)
-        else:
-            print(f"copying {source_item_path} directory to {dest}")
-            copy_dir_content(source_item_path, f"{dest}/{item}")
-
 
 if __name__ == "__main__":
     source = "./public"
