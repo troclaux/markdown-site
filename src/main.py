0e6e7467afb75afc0b46491410f82b96dd3b373a
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-04-15 11:30:13 -0300

    feat: main copies static content and converts md to html

--- src/main.py
+++ src/main.py
@@ -2,7 +2,11 @@ from generate_html_page import *
 from textnode import *
 
 if __name__ == "__main__":
-    source = "./public"
-    destination = "./test"
+    blob_src_path = "./static"
+    blob_dest_path = "./public/static"
+    copy_dir_content(blob_src_path, blob_dest_path)
 
-    copy_dir_content(source, destination)
+    src_path = "./content"
+    template = "./template.html"
+    dest = "./public"
+    generate_pages_recursive(src_path, template, dest)
